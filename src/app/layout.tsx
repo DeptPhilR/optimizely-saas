996e80a0718fc2fe4655e149e692e083e07801fd
Phil Robinson <phil.robinson@deptagency.com>
2024-09-16 15:57:59 +0100

    reapply google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import Script from 'next/script';
 import { type PropsWithChildren } from 'react';
@@ -10,6 +9,7 @@ import Header from '../components/organisms/Header/index';
 
 // Styling
 import { Figtree } from 'next/font/google';
+import Head from 'next/head';
 import './global/index.scss';
 
 const figtree = Figtree({ subsets: ['latin'] });
@@ -28,15 +28,35 @@ type RootLayoutProps = Readonly<PropsWithChildren<{}>>;
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-TDNQ3PPP" />
-      <Script src="https://cdn.optimizely.com/js/30057130179.js" data-test="script" />
-        
+      <Head>
+        <Script src="https://cdn.optimizely.com/js/30057130179.js" data-test="script" />
+        <Script src="foobar" />
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TDNQ3PPP');
+          `,
+          }}
+        />
+      </Head>
 
       <ThemeProvider value={{ theme: 'system' }}>
         <Body
           className={`${figtree.className} bg-ghost-white text-vulcan dark:bg-vulcan dark:text-ghost-white tw-overflow-x-hidden`}
         >
-         
+          <iframe
+            // <!-- Google Tag Manager -->
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TDNQ3PPP"
+            height="0"
+            width="0"
+            className="tw-invisible tw-hidden"
+          ></iframe>
           <div className="flex min-h-screen flex-col justify-between">
             <Header />
             {children}
